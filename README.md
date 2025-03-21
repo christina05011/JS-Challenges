99fcde89731390deae5812441d96743bc887d544
christina05011 <christinaarantxac@gmail.com>
2025-03-21 10:33:24 -0500

    :sparkles: feat: Add README and Variables Challenges

---
+++ README.md
@@ -0,0 +1,18 @@
+✨ JS Challenger website for beginners:
+    > https://www.jschallenger.com/
+
+✨ JS Basics Challenges:
+    We will focus only in JS Basic challenges.
+    > https://www.jschallenger.com/javascript-basics/
+
+- Section: Variables
+    > 1. Print Hello World
+    > 2. Reassign a value to a variable
+    > 3. Assign a value to a variable
+    > 4. Assign the value of another variable
+    > 5. Create the missing variable
+    > 6. Accessing a variable (1): Correct name of variable
+    > 7. Accessing a variable (2): First, declare the variable and then print it
+    > 8. Declare a variable and assign a number (using const)
+    > 9. Reassign a value to a variable (2) (using let instead of const)
+
---
+++ VariablesChll.js
@@ -0,0 +1,86 @@
+/* 
+---- Challenge 1
+Print Hello World
+*/
+
+console.log('---- Challenge 1');
+console.log('Hello world');
+
+/* 
+---- Challenge 2
+Reassign a value to a variable
+*/
+
+console.log('---- Challenge 2');
+let num = 1; // First value
+console.log(num);
+num = 2; // Value reassigned
+console.log(num); // Second value
+
+/* 
+---- Challenge 3
+Assign a value to a variable
+*/
+
+console.log('---- Challenge 3');
+let number;
+number = 8; // Value assigned
+console.log(number);
+
+/* 
+---- Challenge 4
+Assign the value of another variable
+*/
+
+console.log('---- Challenge 4');
+let numOne = 5;
+let numTwo = numOne; // Another value assigned
+console.log(numTwo);
+
+/* 
+---- Challenge 5
+Create the missing variable
+*/
+
+console.log('---- Challenge 5');
+let numFirst = 16; // Create the variable
+let numSecond = numOne;
+console.log(numTwo);
+
+/* 
+---- Challenge 6
+Accessing a variable (1): Correct name of variable
+*/
+
+console.log('---- Challenge 6');
+const numConst = 5;
+console.log(numConst); // Access to the correct variable instead of 'NumConst'
+
+/* 
+---- Challenge 7
+Accessing a variable (2): First, declare the variable and then print it
+*/
+
+console.log('---- Challenge 7');
+// We reordered these two lines to have the declaration first and then the print
+const num_ = 5;
+console.log(num_);
+
+/* 
+---- Challenge 8
+Declare a variable and assign a number (using const)
+*/
+
+console.log('---- Challenge 8');
+const new_num = 800; // New variable with value
+console.log('The value of num is: ' + new_num);
+
+/* 
+---- Challenge 9
+Reassign a value to a variable (2) (using let instead of const)
+*/
+
+console.log('---- Challenge 9');
+let numb = 1; // Change 'const' to 'let'
+numb = 2;
+console.log(numb);
